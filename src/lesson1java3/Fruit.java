f1e3c30528591bb247ac02d22d928c9264044076
4eburek-hab <piton4ik89@gmail.com>
2021-03-09 16:52:57 +0300

    Урок 1 java 3

---
+++ src/lesson1java3/Fruit.java
@@ -0,0 +1,5 @@
+package lesson1java3;
+
+public abstract class Fruit {
+    abstract float getWeight();
+}
---
+++ src/lesson1java3/Orange.java
@@ -0,0 +1,8 @@
+package lesson1java3;
+
+public class Orange extends Fruit {
+    @Override
+    float getWeight() {
+        return 1.5f;
+    }
+}
