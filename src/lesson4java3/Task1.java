7e641ec840b0e7ec14f47189da161c5456f6189a
4eburek-hab <piton4ik89@gmail.com>
2021-03-19 14:56:18 +0300

    lesson 4 java 3

---
+++ src/lesson4java3/Task1.java
@@ -0,0 +1,43 @@
+package lesson4java3;
+
+public class Task1 {
+    static class WaitNotifyClass implements Runnable {
+
+        static volatile char c = 'A';
+        static Object mon = new Object();
+
+        private char currentLetter;
+        private char nextLetter;
+
+        public WaitNotifyClass(char currentLetter, char nextLetter) {
+            this.currentLetter = currentLetter;
+            this.nextLetter = nextLetter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 5; i++) {
+                synchronized (mon) {
+                    try {
+                        while (c != currentLetter)
+                            mon.wait();
+                        System.out.println(currentLetter);
+                        c = nextLetter;
+                        mon.notifyAll();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Task1");
+        new Thread(new WaitNotifyClass('A', 'B')).start();
+        new Thread(new WaitNotifyClass('B', 'C')).start();
+        new Thread(new WaitNotifyClass('C', 'A')).start();
+    }
+}
